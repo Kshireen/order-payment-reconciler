d3a5bbdf803da04c977deb26df8d9bf954a711e5
Ruslan <kshireen30@gmail.com>
2026-09-01 19:11:33 +0530

    fix: fix the import LogoutButton from "@/components/LogoutButton";

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,3 @@
-// import type { Metadata } from "next";
-import Link from "next/link";
-import LogoutButton from "@/components/LogoutButton";
-// import "./globals.css";
-
 import type { Metadata } from "next";
 import AuthNav from "@/components/AuthNav";
 import "./globals.css";
@@ -16,13 +11,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        {/* <nav className="topnav">
-          <Link href="/dashboard">Dashboard</Link>
-          <Link href="/upload">Upload data</Link>
-          <Link href="/login">Login</Link>
-          <Link href="/signup">Sign up</Link>
-          <LogoutButton />
-        </nav> */}
         <AuthNav />
         {children}
       </body>
